7e1829eee94cd53b9331227b1def29077ec44521
Chris Vogel <gihub@z9.de>
2024-10-17 17:32:52 +0200

    make unsafe - change name

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,13 +1,10 @@
 # Verify that the requested reverse proxy destination is valid:
 #   - protocol is http(s):// or unix: for socket file
 #   - plaintext http is only allowed to localhost (to avoid leaking credentials on the network)
+# → removed the above part: there are valid use cases where the internal connection is http to a different
+#   IP. E.g. domain validation of Let's encrypt.
 #   - http(s) destination is webroot, no additional component allowed (eg. http://localhost:1234/test is invalid)
 rp_validate_proxy_path() {
-    if [[ ! $proxy_path =~ '^unix:/' ]]; then
-        url_regex='^(http://(127\.[0-9]+\.[0-9]+\.[0-9]+|localhost)|https://.*)(:[0-9]+)?(/.*)?$'
-        [[ ! $proxy_path =~ $url_regex ]] && ynh_die \
-        "For secure reason, you can't use an unencrypted http remote destination couple with ssowat for your reverse proxy: $proxy_path" 1
-    fi
 
     # Don't allow trailing slash or additional URI components in proxy_path
     if [[ "$proxy_path" =~ ^https?:// ]]; then
